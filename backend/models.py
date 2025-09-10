5e75a7527689274813eb1a816bc547dc8ee45d16
AdamKJZ5 <adamkaljemal@gmail.com>
2025-09-10 14:59:35 -0700

    _innit_, dict_to, dict_from created

--- backend/models.py
+++ backend/models.py
@@ -1,19 +1,43 @@
-from datetime import date
+from datetime import datetime
+
+ALLOWED_CATEGORIES = ["Food", "Transport", "Bills", "Shopping", "Entertainment", "Other"]
 
 class Expense:
-    def __init__(self, item, amount, category, recurring=False, day=None):
+    def __init__(self, id, item, date, amount, category, recurring=False, note=""):
+        self.id = id
         self.item = item
+        self.date = date
         self.amount = amount
         self.category = category
         self.recurring = recurring
-        self.date = day or str(date.today())
+        self.note = note.strip() if note else ""
 
+        now = datetime.utcnow().isoformat()
+        self.created_at = now
+        self.updated_at = now
+        
     def to_dict(self):
         return{
+            "id": self.id,
             "item": self.item,
+            "date": self.date,
             "amount": self.amount,
-            "catagory": self.catagory,
+            "category": self.category,
             "recurring": self.recurring,
-            "date": self.date
+            "note": self.note,
+            "created_at": self.created_at
+            "updated_at": self.updated_at
         }
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            id = data["id"],
+            item = data["item"],
+            date = data["date"],
+            amount = data["amount"],
+            category = data["category"],
+            recurring = data.get("recurring", False),
+            note = data.get("note", "")   
+        )
 
